d11d409732edc28218dcf40c35362dc4dd428d6b
Nurislam <gggrrraaa1235@gmail.com>
2024-03-11 16:03:40 +0500

    5th commit at 58:46 video part-1

--- newsapi/src/main/java/com/example/newsapi/NewsApi.kt
+++ newsapi/src/main/java/com/example/newsapi/NewsApi.kt
@@ -1,6 +1,18 @@
 package com.example.newsapi
 
-import kotlinx.serialization.SerialName
+import androidx.annotation.IntRange
+import com.example.newsapi.models.Article
+import com.example.newsapi.models.Language
+import com.example.newsapi.models.Response
+import com.example.newsapi.models.SortBy
+import com.example.newsapi.utils.NewsApiKeyInterceptor
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.skydoves.retrofit.adapters.result.ResultCallAdapterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.create
 import retrofit2.http.GET
 import retrofit2.http.Query
 import java.util.Date
@@ -8,29 +20,49 @@ import java.util.Date
 interface NewsApi {
 
         @GET("/everything")
-        fun everything(
+       suspend fun everything(
             @Query("q") query: String? = null,
-            @Query("from") date: Date? = null,
+            @Query("from") from: Date? = null,
             @Query("to") to: Date? = null,
-            @Query("to") languages: String? = null,
+            @Query("languages") languages: List<Language>? = null,
             @Query("sortBy") sortBy: SortBy? = null,
-            @Query("pageSize") pageSize: Int = 100,
-            @Query("page") page: Int = 1
+            @Query("pageSize") @IntRange(from =  0, to = 100) pageSize: Int = 100,
+            @Query("page") @IntRange(from = 1) page: Int = 1
 
-        )
-
-}
-enum class SortBy{
-
-    @SerialName("relevancy")
-    RELEVANCY,
-
-    @SerialName("popularity")
-    POPULARITY,
-
-    @SerialName("publishedAt")
-    PUBLISHED_AT,
-
-
+        ): Result<Response<Article>>
 
 }
+
+
+fun newsApi(
+    baseUrl: String,
+    apiKey: String,
+    okHttpClient: OkHttpClient? = null,
+    json: Json = Json
+): NewsApi{
+
+    return retrofit(baseUrl,apiKey,okHttpClient,json).create()
+}
+
+@Suppress("SuspiciousIndentation")
+private fun retrofit(baseUrl: String,
+                     apiKey: String,
+                     okHttpClient: OkHttpClient? = null,
+                     json: Json,
+) : Retrofit {
+
+    val jsonConverterFactory = Json.asConverterFactory(MediaType.get("application/json"))
+
+    val modifiedOkHttpClient: OkHttpClient =  (okHttpClient?.newBuilder() ?: OkHttpClient.Builder()).
+    addInterceptor(NewsApiKeyInterceptor(apiKey)).build()
+
+
+    return Retrofit.Builder().baseUrl(baseUrl)
+        .addConverterFactory(jsonConverterFactory)
+        .addCallAdapterFactory(ResultCallAdapterFactory.create())
+        .client(modifiedOkHttpClient).
+        build()
+}
+
+
+
