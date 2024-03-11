01ff8628eb30c1f0acbf20cea70f8b49dfb4f102
Nurislam <gggrrraaa1235@gmail.com>
2024-03-11 14:19:49 +0500

    4th commit at 28:55 video part-1

--- newsapi/src/main/java/com/example/newsapi/NewsApi.kt
+++ newsapi/src/main/java/com/example/newsapi/NewsApi.kt
@@ -1,4 +1,36 @@
 package com.example.newsapi
 
-class NewsApi {
+import kotlinx.serialization.SerialName
+import retrofit2.http.GET
+import retrofit2.http.Query
+import java.util.Date
+
+interface NewsApi {
+
+        @GET("/everything")
+        fun everything(
+            @Query("q") query: String? = null,
+            @Query("from") date: Date? = null,
+            @Query("to") to: Date? = null,
+            @Query("to") languages: String? = null,
+            @Query("sortBy") sortBy: SortBy? = null,
+            @Query("pageSize") pageSize: Int = 100,
+            @Query("page") page: Int = 1
+
+        )
+
+}
+enum class SortBy{
+
+    @SerialName("relevancy")
+    RELEVANCY,
+
+    @SerialName("popularity")
+    POPULARITY,
+
+    @SerialName("publishedAt")
+    PUBLISHED_AT,
+
+
+
 }
